da2871b3b6f22e2479c73373501e311e7251661d
antonlovric <antonlovric7@gmail.com>
2022-07-14 11:39:58 +0200

    model: added payment and payment type models

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'paid_amount',
+        'payment_type_id',
+        'payment_secret',
+    ];
+
+    public function payment_type()
+    {
+        return $this->belongsTo(PaymentType::class);
+    }
+}
