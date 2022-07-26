eef984df1987387a11e06001e85807bf5cff88db
Anton Lovrić <antonlovri81@gmail.com>
2022-07-27 01:37:48 +0200

    added some migrations

---
+++ database/migrations/2022_07_26_204308_add_cart_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId("cart_id")->constrained("carts")->onDelete("cascade");
+            $table->dropColumn("total");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->dropConstrainedForeignId("cart_id");
+            $table->integer("total");
+        });
+    }
+};
